52819059ccc5b04547586c0fb0d76ac06f42f818
Neville Ingram <nlingram@wpi.edu>
2022-04-18 16:47:54 -0400

    Added SVC

--- models/svc.py
+++ models/svc.py
@@ -1,7 +1,7 @@
 import os
+from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 
 # load training & test from csv
 x_train = pd.read_csv('data/train.csv')
@@ -28,8 +28,8 @@ y_train = pd.read_csv('data/train_labels.csv')
 y_train = y_train.loc[:, y_train.columns != 'sequence']
 
 # fit model
-model = RandomForestClassifier().fit(
-    x_train, y_train.values.ravel())  # error fixed using ravel?
+model = SVC().fit(
+    x_train, y_train.values.ravel())
 
 # predict y_test
 y_pred = model.predict(x_test)
@@ -38,5 +38,5 @@ y_pred = model.predict(x_test)
 submission["state"] = y_pred
 
 # write to csv for kaggle submission
-os.makedirs('submissions/random_forests', exist_ok=True)
-submission.to_csv('submissions/random_forests/out.csv', index=False)
+os.makedirs('submissions/svc', exist_ok=True)
+submission.to_csv('submissions/svc/out.csv', index=False)
