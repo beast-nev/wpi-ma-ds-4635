4751b6ad6f77dc16040fc5101e54f559dc046957
Neville Ingram <nlingram@wpi.edu>
2022-04-18 16:22:52 -0400

    Working on random forests

--- models/random_forests.py
+++ models/random_forests.py
@@ -1,1 +1,7 @@
+from turtle import pd
 import numpy as np
+import pandas as pd
+
+x_train = pd.read_csv('data/train.csv')
+x_train = x_train.groupby(np.arange(len(x_train)) // 60).mean()
+print(x_train)
