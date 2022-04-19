4cb7a8b126bf25f390cc0023c499e4b07c7a6f22
Neville <nlingram@wpi.edu>
2022-04-19 09:47:20 -0400

    Add sequential feature selector, test different hyperparameters

--- models/random_forests.py
+++ models/random_forests.py
@@ -1,11 +1,10 @@
-from re import T
-from sklearn.model_selection import RandomizedSearchCV
 import os
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.feature_selection import SelectKBest, mutual_info_classif
+from sklearn.feature_selection import SequentialFeatureSelector
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
@@ -36,20 +35,31 @@ y_train = pd.read_csv('data/train_labels.csv')
 y_train = y_train.loc[:, y_train.columns != 'sequence']
 
 # feature selection
-selector = SelectKBest(f_classif, k=6)
+selector = SelectKBest(mutual_info_classif, k=6)
 x_train = selector.fit_transform(x_train, y_train.values.ravel())
+
+# if_classif
+# Accuracies:  0.6850963844665071
+# Test train split score:  0.6733410345270183
+
+# mustal_class_if
+# Accuracies:  0.6935688658274356
+# Test train split score:  0.6920806058272365
 
 # predictors choosen
 # print(selector.get_feature_names_out())
 # 'sensor_02' 'sensor_04' 'sensor_06' 'sensor_08' 'sensor_10' 'sensor_12'
 
-
 X_train, X_test, Y_train, Y_test = train_test_split(
     x_train, y_train.values.ravel(), test_size=0.3, random_state=42)
 
 # fit model
-model = RandomForestClassifier(n_estimators=500, min_samples_split=5,
+model = RandomForestClassifier(n_estimators=250, min_samples_split=5,
                                min_samples_leaf=2, max_features='sqrt', max_depth=20, bootstrap=True)
+sfs = SequentialFeatureSelector(model, n_features_to_select=3)
+sfs.fit(X_train, Y_train)
+print(sfs.get_feature_names_out())
+print(sfs.get_support())
 print("Accuracies: ", np.mean(cross_val_score(
     model, X_train, Y_train, cv=5)))
 model.fit(X_train, Y_train)
