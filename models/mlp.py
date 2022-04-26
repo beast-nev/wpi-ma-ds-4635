ca717404ca69592def0236244429fbb9006f76d1
Neville Ingram <nlingram@wpi.edu>
2022-04-26 17:45:36 -0400

    MLP cleanup

--- models/mlp.py
+++ models/mlp.py
@@ -1,3 +1,4 @@
+from sklearn.impute import SimpleImputer
 from sklearn.model_selection import GridSearchCV
 import os
 import numpy as np
@@ -9,7 +10,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
 from time import time
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import roc_auc_score, average_precision_score
+from sklearn.metrics import classification_report, roc_auc_score, average_precision_score
 from sklearn.utils import resample
 import warnings
 warnings.simplefilter(action='ignore', category=Warning)
@@ -17,19 +18,6 @@ warnings.simplefilter(action='ignore', category=Warning)
 # load training & test from csv
 x_train_load = pd.read_csv('data/train.csv')
 x_test_load = pd.read_csv('data/test.csv')
-
-# creating submission csv
-submission = pd.DataFrame(
-    columns=["sequence"], data=x_test_load[["sequence"]].groupby(np.arange(len(x_test_load[["sequence"]])) // 60).mean())
-
-# scaling
-scaler = StandardScaler()
-x_train_load = pd.DataFrame(data=scaler.fit_transform(
-    x_train_load), columns=x_train_load.columns)
-x_test_load = pd.DataFrame(data=scaler.fit_transform(
-    x_test_load), columns=x_test_load.columns)
-
-print(x_train_load.columns)
 
 # load training labels from csv
 y_train_load = pd.read_csv('data/train_labels.csv')
@@ -41,17 +29,20 @@ y_train = y_train_load.loc[:, y_train_load.columns != 'sequence']
 sensor_names = ["sensor_00", "sensor_01", "sensor_02", "sensor_03", "sensor_04", "sensor_05", "sensor_06",
                 "sensor_07", "sensor_08", "sensor_09", "sensor_10", "sensor_11", "sensor_12"]
 
+# creating submission csv
+submission = pd.DataFrame(
+    columns=["sequence"], data=x_test_load[["sequence"]].groupby(np.arange(len(x_test_load[["sequence"]])) // 60).mean())
+
 # create new features for the mean,std,max,min,and sum of each sensor values
 x_train = pd.DataFrame()
 x_test = pd.DataFrame()
 
+# create features for mean, lag, std, min, max, median, and iqr
 for i in sensor_names:
     x_train[i+"_mean"] = x_train_load[i].groupby(
         np.arange(len(x_train_load[i])) // 60).mean()
-    x_train[i+"_count"] = x_train_load[i].groupby(
-        np.arange(len(x_train_load[i])) // 60).count()
-    x_train[i+"_cummax"] = x_train_load[i].groupby(
-        np.arange(len(x_train_load[i])) // 60).cummax()
+    x_train[i+"_lag"] = x_train_load[i].groupby(
+        np.arange(len(x_train_load[i])) // 60).shift(1)
     x_train[i+"_std"] = x_train_load[i].groupby(
         np.arange(len(x_train_load[i])) // 60).std()
     x_train[i+"_max"] = x_train_load[i].groupby(
@@ -65,10 +56,8 @@ for i in sensor_names:
 
     x_test[i+"_mean"] = x_test_load[i].groupby(
         np.arange(len(x_test_load[i])) // 60).mean()
-    x_test[i+"_count"] = x_test_load[i].groupby(
-        np.arange(len(x_test_load[i])) // 60).count()
-    x_test[i+"_cummax"] = x_test_load[i].groupby(
-        np.arange(len(x_test_load[i])) // 60).cummax()
+    x_test[i+"_lag"] = x_test_load[i].groupby(
+        np.arange(len(x_test_load[i])) // 60).shift(1)
     x_test[i+"_std"] = x_test_load[i].groupby(
         np.arange(len(x_test_load[i])) // 60).std()
     x_test[i+"_max"] = x_test_load[i].groupby(
@@ -80,15 +69,18 @@ for i in sensor_names:
     x_test[i+"_irq"] = x_test_load[i].groupby(
         np.arange(len(x_test_load[i])) // 60).quantile(0.75) - x_test_load[i].groupby(np.arange(len(x_test_load[i])) // 60).quantile(0.25)
 
-print("Finished creating features")
+# take samples of our data
+x_train = x_train.sample(frac=1.0, random_state=42)
+y_train = y_train.sample(frac=1.0, random_state=42)
+x_test = x_test.sample(frac=1.0, random_state=42)
 
-# sample
-# x_train = x_train.sample(frac=1.0, random_state=42)
+# features names for pca
+feature_names = x_train.columns
 
-# With scaling
-# Accuracy:  0.7838106835117118
-# Average precision score:  0.8904739937106321
-# Roc score:  0.9237435396647342
+# z transformation
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.fit_transform(x_test)
 
 # feature selection & model creation
 mlp = MLPClassifier(activation='logistic', alpha=0.05, batch_size=500,
@@ -106,24 +98,32 @@ mlp = MLPClassifier(activation='logistic', alpha=0.05, batch_size=500,
 # print("Best params: ", grid_search.best_params_)
 # print("Best score: ", grid_search.best_score_)
 
-# print("Finished feature selection")
-
-print("Accuracy: ", np.mean(cross_val_score(
-    mlp, x_train, y_train.values.ravel(), cv=5, n_jobs=-1)))
+# impute nans
+imp = SimpleImputer(missing_values=np.nan, strategy="mean")
+x_train = imp.fit_transform(x_train)
+x_test = imp.fit_transform(x_test)
 
 # fitting for prediction
 mlp.fit(x_train, y_train.values.ravel())
 
+# predict training values for training scoring
 y_pred_train = mlp.predict(x_train)
 
-print("Average precision score: ", average_precision_score(y_train, y_pred_train))
+# compute roc_auc and classification report
 print("Roc score: ", roc_auc_score(y_train, y_pred_train))
+print("Classification report: ", classification_report(
+    y_true=y_train, y_pred=y_pred_train))
 
-# predict y_test
-y_pred = mlp.predict(x_test)
+# test predcition
+y_p = mlp.predict(x_test)
+
+# predict y_test probability
+y_pred = pd.DataFrame(data=mlp.predict_proba(x_test),
+                      columns=["state0", "state1"])
+y_pred["pred"] = np.max(y_pred.values, axis=1)
 
 # make state in submission csv our prediction
-submission["state"] = y_pred
+submission["state"] = y_pred["pred"]
 
 # write to csv for kaggle submission
 os.makedirs('submissions/mlp', exist_ok=True)
